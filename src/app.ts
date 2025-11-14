e066ae46c631d6e4b0ff98d92d15d62ca61da0ea
Lucja111 <waliglu_aehit@students.vizja.pl>
2025-11-14 20:28:42 +0100

    Uncomment userRouter integration in app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import authRouter from './routes/auth.routes';
 import walletRouter from './routes/wallet.routes';
 // import transactionRouter from './routes/transaction.routes';
 // import exchangeRouter from './routes/exchange.routes';
-// import userRouter from './routes/user.routes';
+import userRouter from './routes/user.routes';
 // import rateRoutes from './routes/rate.routes';
 
 const app: Application = express();
@@ -25,7 +25,7 @@ app.use('/api/auth', authRouter);
 app.use('/api/wallets', walletRouter);
 // app.use('/api/transactions', transactionRouter);
 // app.use('/api/exchange', exchangeRouter);
-// app.use('/api/users', userRouter);
+app.use('/api/users', userRouter);
 // app.use('/api/rates', rateRoutes);
 
 export default app;
