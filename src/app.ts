bd759ac20594b4e6c25d7f5aed7d48666bcfe403
Neyantia <167681308+Neyantia@users.noreply.github.com>
2025-11-15 17:11:49 +0100

    Uncomment transactionRouter integration in app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import connectDB from './config/db';
 import healthRouter from './routes/health.routes';
 import authRouter from './routes/auth.routes';
 import walletRouter from './routes/wallet.routes';
-// import transactionRouter from './routes/transaction.routes';
+import transactionRouter from './routes/transaction.routes';
 // import exchangeRouter from './routes/exchange.routes';
 import userRouter from './routes/user.routes';
 import rateRoutes from './routes/rate.routes';
@@ -23,7 +23,7 @@ app.get('/api', (req: Request, res: Response) => {
 app.use('/api/health', healthRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/wallets', walletRouter);
-// app.use('/api/transactions', transactionRouter);
+app.use('/api/transactions', transactionRouter);
 // app.use('/api/exchange', exchangeRouter);
 app.use('/api/users', userRouter);
 app.use('/api/rates', rateRoutes);
